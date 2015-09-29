e3b302070562307dc6dab4bbefc74adf33873b4a
bochatayy <yannick.bochatay@meteo.fr>
2015-09-29 17:57:52 +0200

    Commit initial.

---
+++ tests/tests.js
@@ -0,0 +1,37 @@
+if (typeof require!= "undefined") {
+    
+    require.config({
+        paths: {
+            "stdconstruct": '../JSYG.StdConstruct',
+            "jquery":"../bower_components/jquery/dist/jquery",
+            "jsyg":"../bower_components/jsyg/jsyg",
+            "jsyg-events":"../bower_components/jsyg-events/JSYG.Events"
+        },
+        urlArgs: "bust=" + (+new Date())
+    });
+}
+
+(function(factory) {
+    
+    if (typeof define == 'function' && define.amd) define(["stdconstruct"],factory);
+    else factory(StdConstruct);
+    
+}(function(StdConstruct) {
+
+    module("StdConstruct");
+
+    test("Gestion des fonction standard", function() {     
+        
+        
+        var obj = new StdConstruct();
+        
+        obj.enable();
+        
+        expect(2);
+        
+        equal(obj.enabled, true, "activation du plugin");
+        
+        equal(typeof obj.on, "function", "héritage de Events");
+    });
+    
+}));
